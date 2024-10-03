4d8d388fec3c056ced44de51d22681ec617ee10d
Tatin13 <166537230+Tatin13@users.noreply.github.com>
2024-10-03 02:54:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const getnumber = document.getElementById("getnumber");
+    const frontimg = document.getElementById("frontimg");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+       const value = Math.floor(Math.random() * 150) + 1;
+       values.push(value);
+       images.push(`<img style="width:300px;" src="pics/${value}.jpg" alt="Image is missing!">`);
+
+    }
+    //diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+    getnumber.style.display = "block";
+    frontimg.style.display = "none";
+
+}
